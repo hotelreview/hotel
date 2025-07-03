7694cf37458087b5d0871ef8858a8073e7a5059c
hotelreview <ivanshpichka47@gmail.com>
2025-07-04 00:03:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const stars = document.querySelectorAll("#stars span");
+const stars = document.querySelectorAll(".stars span");
 const form = document.getElementById("feedback-form");
 let selectedRating = 0;
 
-stars.forEach((star) => {
-  star.addEventListener("mouseover", () => {
-    highlightStars(star.dataset.value);
-  });
-
-  star.addEventListener("mouseout", () => {
-    highlightStars(selectedRating);
-  });
-
+stars.forEach(star => {
   star.addEventListener("click", () => {
     selectedRating = parseInt(star.dataset.value);
     highlightStars(selectedRating);
@@ -18,16 +10,15 @@ stars.forEach((star) => {
     if (selectedRating <= 3) {
       form.style.display = "flex";
     } else {
-      window.location.href =
-        "https://www.google.com/maps/place/Sushi+Wroc%C5%82aw,+Restauracja+Sushi+Wroc%C5%82aw+-+BO+Sushi+we+Wroc%C5%82awiu/@51.1111621,17.0199513,17z/data=!4m8!3m7!1s0x470fc3c65c6743a1:0xa337ce2f6423d5ae!8m2!3d51.1111621!4d17.0199513!9m1!1b1!16s%2Fg%2F11vzcln53l?entry=ttu";
+      window.location.href = "https://maps.app.goo.gl/WWB4ZqHCcaKKPsJa8";
     }
   });
 });
 
-function highlightStars(value) {
-  stars.forEach((star) => {
+function highlightStars(rating) {
+  stars.forEach(star => {
     star.classList.remove("selected");
-    if (parseInt(star.dataset.value) <= value) {
+    if (parseInt(star.dataset.value) <= rating) {
       star.classList.add("selected");
     }
   });
@@ -35,11 +26,10 @@ function highlightStars(value) {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const data = new FormData(form);
-  const body = `
-    Проблема: ${data.get("problem")}
-    Ім'я: ${data.get("name")}
-    Контакт: ${data.get("contact")}
-  `;
-  window.location.href = `mailto:tkebuchava16061991@gmail.com?subject=Відгук (1-3 зірки)&body=${encodeURIComponent(body)}`;
+  const reason = document.getElementById("reason").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  const body = `Причина: ${reason}%0AІм'я: ${name}%0AКонтакт: ${email}`;
+  window.location.href = `mailto:tkebuchava16061991@gmail.com?subject=Негативний відгук (1-3 зірки)&body=${body}`;
 });
